f5720cfaf7a1af4234576b51aac1fb94c1a3eaec
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-12-17 18:11:43 +0530

    17.2 complete

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,13 +1,35 @@
-
 import express from "express";
+import db from "@repo/db/client";
 
 const app = express();
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     const paymentInformation = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
     };
+    await db.balance.update({
+        where: {
+            userId: paymentInformation.userId
+        },
+        data: {
+            amount: {
+                increment: paymentInformation.amount
+            }
+        }
+    });
+
+    await db.onRampTransaction.update({
+        where: {
+            token: paymentInformation.token,
+        },
+        data: {
+            status: "Success"
+        }
+    })
+    res.status(200).json({
+        message: "captured"
+    })
     // Update balance in db, add txn
 })
